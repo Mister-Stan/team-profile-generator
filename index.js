aeb64624bdbde91368fee416ebb319bb7918ab7e
Eugeniu Stan <lucianstan87@yahoo.com>
2023-02-27 22:43:27 +0000

    adds the work that was made in the script files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -13,3 +14,5 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+employee = new Employee ("Jared", "Manager", 1, "jared@fakemail.com");
+manager = new Manager (1);
